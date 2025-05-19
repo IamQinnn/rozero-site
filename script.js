8983ab9ea4aae4828a1793af0fd1596efb80a513
IamQinnn <89908520+IamQinnn@users.noreply.github.com>
2025-05-19 22:19:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ let closeBtn = document.querySelector("#btn");
 let searchBtn= document.querySelector(".bx-search");
 
 // toggle sidebar
-closeBtn.addEventListener("click", toggleSidebar);
-searchBtn.addEventListener("click", toggleSidebar);
-
-function toggleSidebar(){
+closeBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
-  menuBtnChange();
-}
+  menuBtnChange();//calling the function(optional)
+});
 
-function menuBtnChange(){
-  if(sidebar.classList.contains("open")){
-    closeBtn.classList.replace("bx-menu","bx-menu-alt-right");
-  }else{
-    closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
-  }
+searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+ }
 }
 
 /* ----------  ROUTER SEDERHANA  ---------- */
